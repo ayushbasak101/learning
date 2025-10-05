36936a4595d855fc9ef9be303753900d7123ef6d
I'M BATMaN <basakayush942@gmail.com>
2025-10-05 15:33:35 +0000

    database connection added

--- ecommerce/server/index.js
+++ ecommerce/server/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import { connectDB } from "./utils/db.js"
 
 dotenv.config()
 
@@ -9,3 +10,5 @@ const PORT=process.env.PORT || 5000
 app.listen(PORT,() => {
   console.log(`server running on ${PORT}`)
 })
+
+connectDB()
---
+++ ecommerce/server/utils/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+
+
+export const connectDB=async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URI,{
+        dbName:"e-commerce"
+      }
+    )
+    console.log("MONGODB is running")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
